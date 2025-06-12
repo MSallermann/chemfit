2c686f2bdbcc7c51d5bfbb02da9715a9e62aad78
Moritz Sallermann <moritzsallermann@gmail.com>
2025-06-12 13:26:02 +0000

    test_lj: now uses atoms_factory!

--- tests/test_lj.py
+++ tests/test_lj.py
@@ -1,39 +1,23 @@
-from ase.io import write
 from ase.calculators.lj import LennardJones
 from ase import Atoms
 import numpy as np
-from pathlib import Path
 
 from scme_fitting.multi_energy_objective_function import MultiEnergyObjectiveFunction
 from scme_fitting.fitter import Fitter
 
 
+class LJAtomsFactory:
+    def __init__(self, r: float):
+        p0 = np.zeros(3)
+        p1 = np.array([r, 0.0, 0.0])
+        self.atoms = Atoms(positions=[p0, p1])
+
+    def __call__(self):
+        return self.atoms
+
+
 def e_lj(r, eps, sigma):
     return 4.0 * eps * ((sigma / r) ** 6 - 1.0) * (sigma / r) ** 6
-
-
-def prepare_data(r_list: list[float], output_folder: Path, eps: float, sigma: float):
-    p0 = np.zeros(3)
-
-    output_folder.mkdir(exist_ok=True)
-
-    paths = []
-    tags = []
-    energies = []
-
-    for r in r_list:
-        p1 = np.array([r, 0.0, 0.0])
-        atoms = Atoms(positions=[p0, p1])
-        energy = e_lj(r, eps=eps, sigma=sigma)
-
-        p = output_folder / f"atoms_lj_{r:.2f}.xyz"
-        write(p, atoms)
-
-        paths.append(p)
-        tags.append(f"lj_{r:.2f}")
-        energies.append(energy)
-
-    return paths, tags, energies
 
 
 def construct_lj(atoms: Atoms):
@@ -52,16 +36,12 @@ def test_lj():
     r_min = 2 ** (1 / 6) * sigma
     r_list = np.linspace(0.925 * r_min, 3.0 * sigma)
 
-    output_folder = Path("./test_lj")
-
-    paths, tags, energies = prepare_data(r_list, output_folder, eps=eps, sigma=sigma)
-
     ob = MultiEnergyObjectiveFunction(
         calc_factory=construct_lj,
         param_applier=apply_params_lj,
-        tag_list=tags,
-        path_or_factory_list=paths,
-        reference_energy_list=energies,
+        tag_list=[f"lj_{r:.2f}" for r in r_list],
+        reference_energy_list=[e_lj(r, eps, sigma) for r in r_list],
+        path_or_factory_list=[LJAtomsFactory(r) for r in r_list],
     )
 
     fitter = Fitter(ob)
