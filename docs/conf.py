d6716b56c1c4bc87664bd47caba297879b607743
Moritz Sallermann <moritzsallermann@gmail.com>
2025-06-12 11:23:12 +0000

    docs: use "furo" theme

--- docs/conf.py
+++ docs/conf.py
@@ -50,5 +50,5 @@ pygments_style = "sphinx"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 html_static_path = ["src/_static"]
