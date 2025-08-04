7da8c2764a328d737f40fffaf8e9b9677482511e
Moritz Sallermann <moritzsallermann@gmail.com>
2025-08-04 14:41:08 +0000

    test_utils: fixed a stupid error
    was comparing lists to tuples

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -12,8 +12,8 @@ def test_check_params():
 
     problematic_params = check_params_near_bounds(params, bounds, relative_tol=1e-2)
     expected = [
-        ["electrostatic.bla.a", 1.0, 0.5, 1.001],
-        ["dispersion", -0.4, 0.2, 2.0],
+        ("electrostatic.bla.a", 1.0, 0.5, 1.001),
+        ("dispersion", -0.4, 0.2, 2.0),
     ]
 
     assert problematic_params == expected
