4a92672946096b2d7ccc7335342779352028c949
Moritz Sallermann <moritzsallermann@gmail.com>
2025-10-04 21:00:44 +0200

    WIP on refactor

--- src/chemfit/abstract_objective_function.py
+++ src/chemfit/abstract_objective_function.py
@@ -1,5 +1,12 @@
+from __future__ import annotations
+
 import abc
-from typing import Any, Protocol, runtime_checkable
+from typing import Any, Callable, Protocol, runtime_checkable
+
+
+@runtime_checkable
+class SupportsGetMetaData(Protocol):
+    def get_meta_data(self) -> dict[str, Any]: ...
 
 
 class ObjectiveFunctor(abc.ABC):
@@ -23,6 +30,52 @@ class ObjectiveFunctor(abc.ABC):
         ...
 
 
-@runtime_checkable
-class SupportsGetMetaData(Protocol):
-    def get_meta_data(self) -> dict[str, Any]: ...
+class QuantityComputer(abc.ABC):
+    def __init__(self):
+        """Initialize the QuantityComputer."""
+
+        self._last_quantities: dict[str, Any] | None = None
+
+    def get_meta_data(self) -> dict[str, Any]:
+        """Get meta data."""
+        return {"last_quantities": self._last_quantities}
+
+    def __call__(self, parameters: dict[str, Any]) -> dict[str, Any]:
+        self._last_quantities = self._compute(parameters)
+        return self._last_quantities
+
+    @abc.abstractmethod
+    def _compute(self, parameters: dict[str, Any]) -> dict[str, Any]:
+        """Compute dictionary of quantities for a given set of new parameters."""
+        ...
+
+
+class QuantityComputerObjectiveFunction(ObjectiveFunctor):
+    def __init__(
+        self,
+        loss_function: Callable[[dict[str, Any]], float] | ObjectiveFunctor,
+        quantity_computer: QuantityComputer,
+    ) -> None:
+        """Initialize the objective function with a quantity computer."""
+
+        super().__init__()
+        self.quantity_computer = quantity_computer
+        self.loss_function = loss_function
+        self._last_loss: float | None = None
+
+    def get_meta_data(self) -> dict[str, Any]:
+        meta_data = {
+            "quantity_computer": self.quantity_computer.get_meta_data(),
+            "last_loss": self._last_loss,
+        }
+
+        if isinstance(self.loss_function, SupportsGetMetaData):
+            meta_data["loss_function"] = self.loss_function.get_meta_data()
+
+        return meta_data
+
+    def __call__(self, parameters: dict[str, Any]) -> float:
+        quantities = self.quantity_computer(parameters)
+        self._last_loss = self.loss_function(quantities)
+
+        return self._last_loss
