41be7bc79e69fc5f1f09ed21bf5d9e7cd46a558c
Moritz Sallermann <moritzsallermann@gmail.com>
2026-01-15 15:11:09 +0100

    wrap_funcs: can now pass ctx if specified

--- src/chemfit/wrap_funcs.py
+++ src/chemfit/wrap_funcs.py
@@ -8,12 +8,18 @@ from chemfit.abstract_objective_function import (
     QuantityComputer,
 )
 
+WrappableObjFunction = (
+    Callable[[dict[str, Any]], float]
+    | Callable[[dict[str, Any], EvaluateContext], float]
+)
+
 
 class WrappedObjectiveFunctor(ObjectiveFunctor):
-    def __init__(self, func: Callable[[dict[str, Any]], float]):
+    def __init__(self, func: WrappableObjFunction, pass_ctx: bool = False):
         """Wrap a generic callable in an objective functor."""
         super().__init__()
         self.func = func
+        self.pass_ctx = pass_ctx
 
     def __call__(
         self, parameters: dict[str, Any], ctx: EvaluateContext | None = None
@@ -21,31 +27,51 @@ class WrappedObjectiveFunctor(ObjectiveFunctor):
         if ctx is None:
             ctx = EvaluateContext()
 
-        loss = self.func(parameters)
-        ctx.loss = loss
+        if self.pass_ctx:
+            ctx.loss = self.func(parameters, ctx)
+        else:
+            ctx.loss = self.func(parameters)
+
         ctx.parameters = parameters
 
         return ctx.loss
 
 
-def to_objective_functor(func: Callable[[dict[str, Any]], float]):
-    return WrappedObjectiveFunctor(func)
+def to_objective_functor(pass_ctx: bool = False):
+    def wrap(func: WrappableObjFunction):
+        return WrappedObjectiveFunctor(func, pass_ctx=pass_ctx)
+
+    return wrap
+
+
+WrappableQuantFunction = (
+    Callable[[dict[str, Any]], dict[str, Any]]
+    | Callable[[dict[str, Any], EvaluateContext], dict[str, Any]]
+)
 
 
 class WrappedQuantityComputer(QuantityComputer):
-    def __init__(self, func: Callable[[dict[str, Any]], dict[str, Any]]):
+    def __init__(self, func: WrappableQuantFunction, pass_ctx: bool = False):
         """Wrap a generic callable in a quantity computer."""
         super().__init__()
         self.func = func
+        self.pass_ctx = pass_ctx
 
     def _compute(
         self,
         parameters: dict[str, Any],
         ctx: EvaluateContext,
     ) -> dict[str, Any]:
-        ctx.quantities = self.func(parameters)
+        if self.pass_ctx:
+            ctx.quantities = self.func(parameters, ctx)
+        else:
+            ctx.quantities = self.func(parameters)
+
         return ctx.quantities
 
 
-def to_quantity_computer(func: Callable[[dict[str, Any]], dict[str, Any]]):
-    return WrappedQuantityComputer(func)
+def to_quantity_computer(pass_ctx: bool = False):
+    def wrap(func: WrappableQuantFunction):
+        return WrappedQuantityComputer(func, pass_ctx=pass_ctx)
+
+    return wrap
