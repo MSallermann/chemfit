7f605e7c184eaf3850de7c937a8f86725b67a3b6
Moritz Sallermann <moritzsallermann@gmail.com>
2026-01-17 19:57:37 +0100

    benchmark: added dask and mpi

--- benchmark/plot_results.py
+++ benchmark/plot_results.py
@@ -21,15 +21,10 @@ for i, p in enumerate(RESULTS_FOLDER.glob("*.json")):
         color=f"C{i}",
     )
 
-    # wtimes_cont = np.linspace(np.min(wtimes), np.max(wtimes), 200)
-
-    # ax.plot(
-    #     wtimes_cont, res["time_slope"] * wtimes_cont + res["time_offset"], color=f"C{i}"
-    # )
-
 ax.set_xscale("log")
 ax.set_yscale("log")
 ax.legend()
 ax.set_ylabel("Total time [s]")
 ax.set_xlabel("Time per task [s]")
+ax.grid(visible=True, which="both", color="lightgrey", ls="-")
 fig.savefig("time_taken.png", dpi=300)
