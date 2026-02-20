ceb060c917ab1a1be898f90df99cda0adca62856
Moritz Sallermann <moritzsallermann@gmail.com>
2026-02-20 10:27:11 +0100

    fitter_callbacks: module with useful callbacks

---
+++ src/chemfit/fitter_callbacks.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+import json
+import logging
+from pathlib import Path
+
+from chemfit.fitter import FitterEvaluateContext
+
+logger = logging.getLogger(__name__)
+
+
+# Logging progress
+def log_progress(step: int, ctxs: list[FitterEvaluateContext]):
+    logger.info("=" * 40)
+    logger.info(f"Step = {step}")
+
+    best_params: dict | None = {}
+    best_loss: float | None = None
+    for ictx, ctx in enumerate(ctxs):
+        logger.info(f"  Context {ictx}")
+        logger.info(f"    Opt loss = {ctx.opt_loss}")
+        logger.info(f"    Opt params = {ctx.opt_params}")
+        logger.info(f"    Cur loss = {ctx.loss}")
+        logger.info(f"    Cur params = {ctx.parameters}")
+
+        if best_loss is None or (ctx.opt_loss is not None and best_loss > ctx.opt_loss):
+            best_loss = ctx.opt_loss
+            best_params = ctx.opt_params
+
+    logger.info(f"  Opt loss (all contexts)   = {best_loss}")
+    logger.info(f"  Best params (all context) = {best_params}")
+    logger.info("-" * 40)
+
+
+class SaveMetaData:
+    def __init__(self, output_folder: Path | str):
+        """Saves the meta data to a folder."""
+        self.output_folder = Path(output_folder)
+        self.output_folder.mkdir(exist_ok=True)
+
+    def __call__(self, step: int, ctxs: list[FitterEvaluateContext]):
+        try:
+            for ictx, ctx in enumerate(ctxs):
+                with (self.output_folder / f"step_{step}_ctx_{ictx}.json").open(
+                    "w"
+                ) as f:
+                    json.dump(
+                        ctx.to_meta_data(),
+                        f,
+                        indent=4,
+                        skipkeys=True,
+                    )
+        except Exception:
+            logger.exception("Exception when trying to save meta data!")
