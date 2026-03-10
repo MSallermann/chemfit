c61bd16dc348c19b85df96223b28a2a473970252
Moritz Sallermann <moritzsallermann@gmail.com>
2026-03-10 14:31:07 +0100

    benchmark: plot_results.py updated

--- benchmark/plot_results.py
+++ benchmark/plot_results.py
@@ -1,62 +1,50 @@
+import argparse
 import json
 from pathlib import Path
 
 import matplotlib.pyplot as plt
-import numpy as np
+import polars as pl
 
-RESULTS_FOLDER = Path("./results")
 
-fig, ax = plt.subplots()
+def main(results_folder: Path, plot_folder: Path):
+    fig, ax = plt.subplots()
 
-for i, p in enumerate(RESULTS_FOLDER.glob("*.json")):
-    with p.open("r") as f:
-        res = json.load(f)
+    for i, p in enumerate(results_folder.glob("*.json")):
+        with p.open("r") as f:
+            res = json.load(f)
 
-    wtimes = np.array(res["params"]["wait_times"])
-    time_taken = np.array(res["time_taken_list"])
-    ax.plot(
-        wtimes,
-        time_taken,
-        label=res["params"]["label"],
-        marker="s",
-        mec="black",
-        ls="None",
-        color=f"C{i}",
-    )
+        time_taken = res["time_taken_list"]
+        df = pl.from_records(time_taken)
 
-    wtimes_cont = np.linspace(np.min(wtimes), np.max(wtimes), 500)
+        ax.plot(
+            df["wait_time"],
+            df["time_taken"],
+            label=res["params"]["label"],
+            marker="s",
+            mec="black",
+            ls="None",
+            color=f"C{i}",
+        )
 
-    ax.plot(
-        wtimes_cont,
-        wtimes_cont * res["time_slope"] + res["time_offset"],
-        marker="None",
-        ls="-",
-        color=f"C{i}",
-    )
+        ax.set_xlabel("Time per task [s]")
+        ax.set_ylabel("Total time [s]")
+        ax.set_xscale("log")
+        ax.set_yscale("log")
 
-    # def cost_func(x: float, a: float, b: float):
-    #     return a * x + b
+    ax.grid(visible=True)
+    ax.legend()
 
-    # popt, pcov = curve_fit(
-    #     cost_func,
-    #     wtimes,
-    #     time_taken,
-    #     sigma=time_taken / np.log(time_taken), # this creates an even weight in log-space
-    #     absolute_sigma=True,
-    # )
+    fig.savefig(plot_folder / "time_taken.png", dpi=600)
 
-    # ax.plot(
-    #     wtimes,
-    #     cost_func(wtimes, *popt),
-    #     marker="None",
-    #     ls="-",
-    #     color=f"C{i}",
-    # )
 
-ax.set_xscale("log")
-ax.set_yscale("log")
-ax.legend()
-ax.set_ylabel("Total time [s]")
-ax.set_xlabel("Time per task [s]")
-ax.grid(visible=True, which="both", color="lightgrey", ls="-")
-fig.savefig("time_taken.png", dpi=300)
+if __name__ == "__main__":
+    cli = argparse.ArgumentParser()
+    cli.add_argument("-i", type=Path, required=True)
+    cli.add_argument("-o", type=Path, required=True)
+
+    args = cli.parse_args()
+    input_folder = Path(args.i)
+    output_folder = Path(args.o)
+    output_folder.mkdir(exist_ok=True)
+
+    main(results_folder=input_folder, plot_folder=output_folder)
